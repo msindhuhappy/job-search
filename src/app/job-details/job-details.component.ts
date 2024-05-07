0e7a96e6cd2305eb081895f165beaab0b56a14f1
msindhuhappy <167251004+msindhuhappy@users.noreply.github.com>
2024-05-07 22:07:50 +0530

    page refresh changes

--- src/app/job-details/job-details.component.ts
+++ src/app/job-details/job-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { JobDescription } from '../job.interface';
 import { CommonModule } from '@angular/common';
 import { JobDetailsService } from '../job-details.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 @Component({
@@ -14,15 +14,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class JobDetailsComponent implements OnInit {
   jobDescription!: JobDescription;
-  constructor(private js: JobDetailsService, private router: Router, private location: Location) { }
+  constructor(private js: JobDetailsService, private router: Router, private route: ActivatedRoute, private location: Location) { }
   http = inject(HttpClient)
   ngOnInit(): void {
-    const id = this.js.SelectedJobItem.id;
+    let id: number;
+    if (this.route.snapshot.paramMap.get('id')) {
+      id = Number(this.route.snapshot.paramMap.get('id'));
+    } else {
+      id = this.js.SelectedJobItem.id;
+    }
     this.js.getJobDescrition(id).subscribe((data => {
       this.jobDescription = data;
     }));
-
-
   }
   goBack() {
     this.location.back();
