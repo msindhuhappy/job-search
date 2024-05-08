fa6b89bdb2fa68c9677b6869706653a65f1e3f04
msindhuhappy <167251004+msindhuhappy@users.noreply.github.com>
2024-05-09 01:00:48 +0530

    Add files via upload

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -16,22 +16,26 @@ export class JobsComponent implements OnInit {
   jobList: JobDetail[] = [];
   favorites: JobDetail[] = [];
   isSelected: boolean = false;
-  constructor(private jobService: JobDetailsService, private router: Router) {  }
+  savedJobsList:JobDetail[]=[]
+  constructor(private jobService: JobDetailsService, private router: Router) {
+
+  }
   ngOnInit(): void {
     if (this.jobService.SelectedJobList.length !== 0) {
       this.jobList = this.jobService.wholeJobList;
-      localStorage.setItem("wholeJobList", JSON.stringify(this.jobList))
+      localStorage.setItem("wholeJobList",JSON.stringify(this.jobList))
     }
     else {
       this.getJobList();
     }
+    this.loadJobFromLocal();
     console.log("isFavSelected", this.jobService.isFavSelected);
   }
   getJobList() {
     this.jobService.getJobDetails().subscribe(res => {
       this.jobList = res;
       this.jobService.wholeJobList = this.jobList;
-      localStorage.setItem("wholeJobList", JSON.stringify(this.jobList))
+      localStorage.setItem("wholeJobList",JSON.stringify(this.jobList))
     })
   }
   removeDuplicates(arr: JobDetail[]): JobDetail[] {
@@ -50,43 +54,140 @@ export class JobsComponent implements OnInit {
     const listItem = this.jobList.filter(x => x.id === job.id);
     if (listItem[0].isFavSelected) {
       listItem[0].isFavSelected = false;
-      localStorage.setItem("isFavSelected", JSON.stringify(listItem[0].isFavSelected))
+      localStorage.setItem("isFavSelected",JSON.stringify(listItem[0].isFavSelected))
     }
     else {
       listItem[0].isFavSelected = true;
-      localStorage.setItem("isFavSelected", JSON.stringify(listItem[0].isFavSelected))
+      localStorage.setItem("isFavSelected",JSON.stringify(listItem[0].isFavSelected))
     }
     if (job.isFavSelected) {
       this.addFavorite(job);
-      localStorage.setItem("isFavSelected", JSON.stringify(job.isFavSelected))
+      localStorage.setItem("isFavSelected",JSON.stringify(job.isFavSelected))
     }
     if (!job.isFavSelected) {
       this.removeFavorite(job);
-      localStorage.setItem("isFavSelected", JSON.stringify(job.isFavSelected))
+      localStorage.setItem("isFavSelected",JSON.stringify(job.isFavSelected))
     }
   }
   addFavorite(job: JobDetail) {
     if (!this.favorites.includes(job)) {
       this.favorites.push(job);
+
       this.jobService.favoriteList = this.favorites;
       this.jobService.SelectedJobList = this.removeDuplicates(this.jobService.favoriteList);
-      localStorage.setItem("favoriteList", JSON.stringify(this.favorites));
-      localStorage.setItem("SelectedJobList", JSON.stringify(this.removeDuplicates(this.jobService.favoriteList)));
+      localStorage.setItem("favoriteList",JSON.stringify(this.favorites));
+      localStorage.setItem("SelectedJobList",JSON.stringify(this.removeDuplicates(this.jobService.favoriteList)));
     }
   }
   removeFavorite(job: JobDetail) {
     this.favorites = this.jobService.SelectedJobList.filter(favorite => favorite.id !== job.id)
     console.log("fav List", this.favorites);
+    // const jobIn=this.jobList.find(j=>j.id===job.id);
+    // if(jobIn){
+    //   jobIn.isFavSelected=false;
+    // }    
     job.isFavSelected = false;
+
+
     this.jobService.favoriteList = this.favorites;
     this.jobService.SelectedJobList = this.jobService.favoriteList;
-    localStorage.setItem("favoriteList", JSON.stringify(this.favorites));
-    localStorage.setItem("SelectedJobList", JSON.stringify(this.jobService.favoriteList));
+    localStorage.setItem("favoriteList",JSON.stringify(this.favorites));
+      localStorage.setItem("SelectedJobList",JSON.stringify(this.jobService.favoriteList));
+    
   }
+  // favoriteSelected(data: JobDetail) {
+  //   const listItem = this.jobList.filter(x => x.id === data.id);
+  //   if (listItem[0].isFavSelected) {
+  //     listItem[0].isFavSelected = false;
+  //   }
+  //   else {
+  //     listItem[0].isFavSelected = true;
+  //   }
+  //   this.favorites.push(data);      
+  //   data.isFavSelected=true;
+  //   this.jobService.isFavSelected=data.isFavSelected;
+  //   this.jobService.favoriteList=this.favorites;
+  //   this.jobService.SelectedJobList=this.removeDuplicates(this.jobService.favoriteList);
+  //   console.log("fav List", this.jobService.favoriteList);
+  //   console.log("selected List", this.jobService.SelectedJobList);
+  //   console.log("status icon List", this.jobService.isFavSelected);
+  // }
 
-  jobDetail(job: JobDetail, jobId: number) {
+  // removeFavorites(data:JobDetail){
+  //   data.isFavSelected=false;
+  //   this.jobService.isFavSelected=data.isFavSelected;
+  //   this.favorites=this.favorites.filter(y => y.id !== data.id);
+  //   this.jobService.favoriteList=this.favorites;
+  //   console.log("fav List", this.jobService.favoriteList);
+  //   console.log("selected List", this.jobService.SelectedJobList);
+  //   console.log("status icon List", this.jobService.isFavSelected);
+  // }
+
+  jobDetail(job: JobDetail,jobId:number) {
     this.jobService.SelectedJobItem = job;
-    localStorage.setItem("SelectedJobItem", JSON.stringify(job));
-    this.router.navigate(['/jobDetails', jobId]);
+    localStorage.setItem("SelectedJobItem",JSON.stringify(job));
+    this.router.navigate(['/jobDetails',jobId]);
+  }
+  addToFavorites(job:JobDetail){
+    job.isFavSelected=!job.isFavSelected;
+    if(job.isFavSelected){
+      if(!this.favorites.includes(job)){
+        this.favorites.push(job);
+        this.jobService.favoriteList=this.favorites;
+        this.jobService.favoriteList = this.favorites;
+        this.jobService.SelectedJobList = this.removeDuplicates(this.jobService.favoriteList);
+        localStorage.setItem("favoriteList",JSON.stringify(this.favorites));
+        localStorage.setItem("SelectedJobList",JSON.stringify(this.removeDuplicates(this.jobService.favoriteList)));
+     
+      }
+    }else{
+      // const index=this.jobService.SelectedJobList.findIndex(favoriteJob=> favoriteJob.id === job.id);
+      // if(index !== -1){
+      //   // this.favorites.splice(index,1); 
+      //   // this.jobService.favoriteList = this.favorites;
+      //   // this.jobService.SelectedJobList = this.jobService.favoriteList;
+      //   // localStorage.setItem("favoriteList",JSON.stringify(this.favorites));
+      //   //   localStorage.setItem("SelectedJobList",JSON.stringify(this.jobService.favoriteList)); 
+         
+      // }
+       this.favorites = this.jobService.SelectedJobList.filter(favorite => favorite.id !== job.id)
+    console.log("fav List", this.favorites);
+    // const jobIn=this.jobList.find(j=>j.id===job.id);
+    // if(jobIn){
+    //   jobIn.isFavSelected=false;
+    // }    
+    job.isFavSelected = false;
+
+
+    this.jobService.favoriteList = this.favorites;
+    this.jobService.SelectedJobList = this.jobService.favoriteList;
+    localStorage.setItem("favoriteList",JSON.stringify(this.favorites));
+      localStorage.setItem("SelectedJobList",JSON.stringify(this.jobService.favoriteList));
+    }
+this.saveJobtoLocal();
+  }
+  private loadJobFromLocal(){
+    const selectedJobList=localStorage.getItem('SelectedJobList');
+    if(selectedJobList){
+    this.savedJobsList= JSON.parse(selectedJobList);
+    }
+    this.jobList.forEach(job=>{
+      const savedJob=this.savedJobsList.find(savedJob => savedJob.id === job.id);
+      if(savedJob){
+        job.isFavSelected=savedJob.isFavSelected;
+        if(job.isFavSelected){
+          this.favorites.push(job);
+          this.jobService.favoriteList = this.favorites;
+          this.jobService.SelectedJobList = this.jobService.favoriteList;
+          localStorage.setItem("favoriteList",JSON.stringify(this.favorites));
+            localStorage.setItem("SelectedJobList",JSON.stringify(this.jobService.favoriteList));
+        }
+      }
+    })
+  }
+  private saveJobtoLocal(){
+const savedJobs=this.jobList.map(job => ({id:job.id, isFavSelected:job.isFavSelected}));
+localStorage.setItem("SelectedJobList",JSON.stringify(savedJobs));
+
   }
 }
